9b58265f58e9d78c1baadc09d258e9f56a1066ef
stefan <mail@stefanh.de>
2015-08-04 16:27:18 +0200

    support spaces within dockerfile path

--- src/main/groovy/se/transmode/gradle/plugins/docker/client/NativeDockerClient.groovy
+++ src/main/groovy/se/transmode/gradle/plugins/docker/client/NativeDockerClient.groovy
@@ -30,23 +30,21 @@ class NativeDockerClient implements DockerClient {
     @Override
     void buildImage(File buildDir, String tag) {
         Preconditions.checkArgument(tag as Boolean,  "Image tag can not be empty or null.")
-        def cmdLine = "${binary} build -t ${tag} ${buildDir}"
-        executeAndWait(cmdLine)
+        executeAndWait(binary, 'build', '-t', tag, buildDir.toString())
     }
 
     @Override
     void pushImage(String tag) {
         Preconditions.checkArgument(tag as Boolean,  "Image tag can not be empty or null.")
-        def cmdLine = "${binary} push ${tag}"
-        executeAndWait(cmdLine)
+        executeAndWait(binary, 'push', tag)
     }
 
-    private static void executeAndWait(String cmdLine) {
-        def process = cmdLine.execute()
+    private static void executeAndWait(String... commands) {
+        def process = commands.execute()
         process.consumeProcessOutput(System.out as OutputStream, System.err)
         process.waitFor()
         if (process.exitValue()) {
-            throw new GradleException("Docker execution failed\nCommand line [${cmdLine}] returned:\n${process.err.text}")
+            throw new GradleException("Docker execution failed\nCommand line [${commands}] returned:\n${process.err.text}")
         }
     }
 
